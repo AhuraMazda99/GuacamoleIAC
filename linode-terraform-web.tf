bcc7ed268b3e9b14cd2a70de73d17dcbecc999da
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-23 09:26:55 +0100

    testing terraform on linode

---
+++ linode-terraform-web.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "1.27.1"
+    }
+  }
+}
+
+provider "linode" {
+  token = "72c992d857dc90563d0b6278d8c1eaeaa9e3c41ddb0ba0dec9e7e710ca733e38"
+}
+
+resource "linode_instance" "terraform-web" {
+        image = "linode/ubuntu18.04"
+        label = "Terraform-Web-Example"
+        group = "Terraform"
+        region = "us-east"
+        type = "g6-standard-1"
+        authorized_keys = [ "YOUR_PUBLIC_SSH_KEY" ]
+        root_pass = "Jizanisthebest123!"
+}

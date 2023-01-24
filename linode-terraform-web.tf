5b3f39d876af720ad42141bd2b91f9e282311011
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-24 11:32:42 +0100

    testing terraform env varibale injection

--- linode-terraform-web.tf
+++ linode-terraform-web.tf
@@ -7,12 +7,15 @@ terraform {
   }
 }
 
-provider "linode" {
-  token = data.linode_account.Current.token
-}
-data "linode_account" "Current" {
-}
 
+provider "linode" {
+  token = var.token
+}
+variable "token" {
+  type = string
+  description = "token"
+  default = ""
+}
 resource "linode_instance" "terraform-web" {
         image = "linode/ubuntu18.04"
         label = "Terraform-Web-Example"

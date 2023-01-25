2bb47101bb01e12a64b00b433ded4330abe2d395
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-25 11:47:14 +0100

    making var more secure

--- linode-terraform-web.tf
+++ linode-terraform-web.tf
@@ -16,12 +16,17 @@ variable "token" {
   description = "token"
   default = ""
 }
+variable "sshkey" {
+  type = string
+  description = "token"
+  default = ""
+}
 resource "linode_instance" "terraform-web" {
         image = "linode/ubuntu18.04"
         label = "Terraform-Web-Example"
         group = "Terraform"
         region = "us-east"
         type = "g6-standard-1"
-        authorized_keys = [ "YOUR_PUBLIC_SSH_KEY" ]
+        authorized_keys = var.sshkey
         root_pass = "test"
 }
